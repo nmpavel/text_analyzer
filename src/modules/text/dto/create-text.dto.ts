cdd5ec39215a514f8f64d05ad402720baa79df09
devsevenneutrix <devseven@neutrix.com>
2024-08-13 22:47:07 +0600

    Frontend added

--- src/modules/text/dto/create-text.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsNotEmpty } from "class-validator";
-
-export class CreateTextDto {
-    @IsNotEmpty()
-    readonly content: string;
-}
--- src/modules/text/dto/update-text.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateTextDto } from "./create-Text.dto";
-
-export class UpdateTextDto extends PartialType(CreateTextDto) {
-}
