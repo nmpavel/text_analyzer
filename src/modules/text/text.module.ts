9681d9d2391056e9aae6079176c319694b72cf8b
devsevenneutrix <devseven@neutrix.com>
2024-08-14 13:24:00 +0600

    Caching and throttling added

--- src/modules/text/text.module.ts
+++ src/modules/text/text.module.ts
@@ -3,10 +3,12 @@ import {TextController} from "./Text.controller";
 import {TextService} from "./Text.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { TextSchema } from "./schema/Text.schema";
+import { CacheModule } from "@nestjs/cache-manager";
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: 'Text', schema: TextSchema }]),
+        CacheModule.register(),
     ],
     controllers: [TextController],
     providers: [TextService]
