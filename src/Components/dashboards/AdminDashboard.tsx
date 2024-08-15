453a59a26bbe088a4a9805715e4fbe1669cc1836
devsevenneutrix <devseven@neutrix.com>
2024-08-15 23:16:21 +0600

    Created different dashboards for user and admin

--- src/Components/dashboards/AdminDashboard.tsx
+++ src/Components/dashboards/AdminDashboard.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import ApiService from '@/utils/apiCore';
+import React, { useEffect, useState } from 'react'
 
 const AdminDashboard = () => {
+    const [texts,setTexts]=useState<any[]>([]);
+    const apiService = new ApiService ();
+    useEffect(()=>{
+        fetchTexts();
+    },[])
+    const fetchTexts = async () => {
+       try{
+        const resp = await apiService.getData("text");
+        if(resp.statusCode===200) setTexts(resp?.data)
+       }catch (err){
+        console.log(err)
+       }
+    }
   return (
-    <div>AdminDashboard</div>
+    <div className=' bg-white p-6'>
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-6'>
+        {
+        texts.map((text:any)=>(
+            <div className=' bg-gray-200 p-4 rounded-lg shadow-md'>
+                <p className=' text-black'>{text?.content}</p>
+            </div>
+        ))
+       }
+        </div>
+    </div>
   )
 }
 
